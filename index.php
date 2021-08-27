93a0c8f8e84fd90fe66ff16c5a60dadb9a48b7ac
bradyinnovation <bradyincorporated2@gmail.com>
2021-08-27 07:58:16 -0500

    headers

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <html>
 <body>
+<?php 
+header('Access-Control-Allow-Origin: *');
 
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+?>
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 
